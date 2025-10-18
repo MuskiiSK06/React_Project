85e76c796832a9641062c128203ee7428e0b214f
Muskii Shaikh <muskiisk06@gmail.com>
2025-10-18 18:10:33 +0530

    React basic done

---
+++ react-basic-01/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+   <>
+     <div id='parent'>
+      <h1 id='child1'>Hello React</h1>
+      <h2 id='child2'>Welcome Muskan</h2>
+     </div>
+
+     <div id='parent2'></div>
+
+   </>
+  )
+}
+
+export default App

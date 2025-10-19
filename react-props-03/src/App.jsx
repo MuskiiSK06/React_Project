9e3378acddde33c7860fb48e23fd20ce79f9f906
Muskii Shaikh <muskiisk06@gmail.com>
2025-10-19 18:47:36 +0530

    props done

--- react-props-03/src/App.jsx
+++ react-props-03/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
       <Card user="Soniya Ambade" age={22} img="https://images.unsplash.com/photo-1760694121380-0dc12e8ac00f?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=764"/>
 
-      <Card user="Aman Sharma" age={20} img="https://images.unsplash.com/photo-1760465881538-8b17b33b0337?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687"/>
+      <Card user="Aman Sharma" age={20} img="https://images.unsplash.com/photo-1760465881538-8b17b33b0337?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687`"/>
     </div>
   )
 }

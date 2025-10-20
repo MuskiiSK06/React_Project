0c4d98e5bf282ea3ea6e288fef5be577cc7897b4
Muskii Shaikh <muskiisk06@gmail.com>
2025-10-20 23:08:25 +0530

    Installed Tailwind-CSS

---
+++ react-tailwind-06/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='h-screen w-full bg-gray-900'>
+        <h1 className='text-white'>Hello, I am Muskan!</h1>
+    </div>
+  )
+}
+
+export default App
